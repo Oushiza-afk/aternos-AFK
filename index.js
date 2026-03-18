3a546e098e89e2dbdd91785d1716ac9daa1fa2e3
Oushiza-afk <antonantoshkiv@gmail.com>
2026-03-18 19:39:34 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const mineflayer = require('mineflayer');
 
 function createBot() {
     const bot = mineflayer.createBot({
-        host: 'oushiz.aternos.me:64164', // Заміни на свою адресу сервера
+        host: 'oushiz.aternos.me', // Заміни на свою адресу сервера
         port: 64164,
         username: 'AFK_Bot',
         version: false
