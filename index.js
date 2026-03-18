e2dd88acc17eea986d01c5fed7bf211112f95c75
Oushiza-afk <antonantoshkiv@gmail.com>
2026-03-18 19:10:27 +0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,27 @@
+const mineflayer = require('mineflayer');
+
+function createBot() {
+    const bot = mineflayer.createBot({
+        host: 'oushiz.aternos.me:64164', // Заміни на свою адресу сервера
+        port: 64164,
+        username: 'AFK_Bot',
+        version: false
+    });
+
+    bot.on('spawn', () => {
+        console.log('Бот зайшов! Починаю стрибати...');
+        setInterval(() => {
+            bot.setControlState('jump', true);
+            setTimeout(() => bot.setControlState('jump', false), 500);
+        }, 60000); // Стрибок кожну хвилину
+    });
+
+    bot.on('end', () => {
+        console.log('З’єднання розірвано. Перезапуск через 30 сек...');
+        setTimeout(createBot, 30000);
+    });
+
+    bot.on('error', (err) => console.log('Помилка бота:', err));
+}
+
+createBot();
